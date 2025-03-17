a3273b1e1ac8e0904d3be3bd0d4d3c16993cd9a9
mmrazvan <mmrazvan@yahoo.com>
2025-03-17 11:27:28 +0200

    Implement dependency injection in Program.cs
    Refactor `Program.cs` to use `Microsoft.Extensions.DependencyInjection` for managing dependencies. Added a `ServiceProvider` property and a `ConfigureServices` method to register services like `RDSContext`, `IOpisRepo`, `IHeaderRepo`, and `ScanForm`. Updated `ScanApp.csproj` to include the `Microsoft.Extensions.DependencyInjection` package version 9.0.3.

--- ScanApp/Program.cs
+++ ScanApp/Program.cs
@@ -4,6 +4,7 @@ using DataAccess.Models;
 using DataAccess.Repos;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ScanApp;
 
@@ -12,18 +13,31 @@ internal static class Program
 	/// <summary>
 	///  The main entry point for the application.
 	/// </summary>
+	///
+	public static IServiceProvider ServiceProvider { get; private set; }
+
 	[STAThread]
 	private static void Main()
 	{
 		// To customize application configuration such as set high DPI settings or default font,
 		// see https://aka.ms/applicationconfiguration.
 		ApplicationConfiguration.Initialize();
+
+		var serviceCollection = new ServiceCollection();
+		ConfigureServices(serviceCollection);
+
+		ServiceProvider = serviceCollection.BuildServiceProvider();
+		Application.Run(ServiceProvider.GetRequiredService<ScanForm>());
+	}
+
+	public static void ConfigureServices( IServiceCollection services )
+	{
 		var connectionString = ConfigurationManager.ConnectionStrings["RDSContext"].ConnectionString;
-		var options = new DbContextOptionsBuilder<RDSContext>()
-			 .UseSqlServer(connectionString)
-			 .Options;
-		using RDSContext context = new RDSContext(options);
-		IOpisRepo opisRepo = new OpisRepo(context);
-		Application.Run(new ScanForm(context, opisRepo));
+
+		services.AddDbContext<RDSContext>(options => options.UseSqlServer(connectionString), ServiceLifetime.Transient);
+
+		services.AddTransient<IOpisRepo, OpisRepo>();
+		services.AddTransient<IHeaderRepo, HeaderRepo>();
+		services.AddTransient<ScanForm>();
 	}
 }
